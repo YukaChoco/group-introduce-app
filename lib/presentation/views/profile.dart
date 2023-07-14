310ed7cfef3b2da977bea3f58d913df7a4dc54f7
mercy34mercy <etukobamasatyan@gmail.com>
2023-07-14 23:31:19 +0900

    静的解析修正2

---
+++ lib/presentation/views/profile.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:group_introduce_project/domain/model/profile.dart';
+import 'package:group_introduce_project/domain/usecase/profile.dart';
+import 'package:group_introduce_project/presentation/widgets/profile_mini_card_list.dart';
+
+class ProfilePage extends ConsumerWidget {
+  const ProfilePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final profile = ref.watch(getProfileProvider).execute("1");
+    return StreamBuilder<List<Profile>>(
+        stream: profile,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text('An error has occurred'),
+                  TextButton(
+                    child: const Text('Retry'),
+                    onPressed: () {
+                      // Retry logic here
+                    },
+                  ),
+                ],
+              ),
+            );
+          } else {
+            // snapshot.dataがnullである場合に備えて、空のリストをフォールバックとして使用します。
+            final profiles = snapshot.data ?? [];
+            if (profiles.isEmpty) {
+              return const Center(
+                child: Text('No data available'),
+              );
+            } else {
+              return Scaffold(
+                  appBar: AppBar(
+                    backgroundColor:
+                        Theme.of(context).colorScheme.inversePrimary,
+                    title: Text(title),
+                  ),
+                  body: Center(child: ProfileMiniCardList(profiles: profiles)));
+            }
+          }
+        });
+  }
+}
